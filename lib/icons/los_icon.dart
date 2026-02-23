1f816f7e087d5c85be2c763d22bf19a4a11951ab
just_stuff_tm <133525672+just-stuff-tm@users.noreply.github.com>
2026-02-23 01:06:25 -0500

    ran dart format . on libs/icons/los_icon.dart

--- lib/icons/los_icon.dart
+++ lib/icons/los_icon.dart
@@ -5,25 +5,18 @@ class LosIcon extends StatelessWidget {
   final double size;
   final Color? color;
 
-  const LosIcon({
-    super.key,
-    this.size = 24,
-    this.color,
-  });
+  const LosIcon({super.key, this.size = 24, this.color});
 
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
     final iconTheme = IconTheme.of(context);
-    final iconColor = color ??
+    final iconColor =
+        color ??
         iconTheme.color ??
         theme.iconTheme.color ??
         theme.colorScheme.onSurface;
 
-    return Icon(
-      Symbols.elevation,
-      size: size,
-      color: iconColor,
-    );
+    return Icon(Symbols.elevation, size: size, color: iconColor);
   }
 }
