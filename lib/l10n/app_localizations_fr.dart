400b088994e64047809041588ea496b412705b08
Ben Allfree <ben@benallfree.com>
2026-02-24 22:16:10 -0800

    fix localization

--- lib/l10n/app_localizations_fr.dart
+++ lib/l10n/app_localizations_fr.dart
@@ -2919,7 +2919,7 @@ class AppLocalizationsFr extends AppLocalizations {
 
   @override
   String get losElevationAttribution =>
-      'Données d’altitude : Open-Meteo (CC BY 4.0)';
+      'Données d\'altitude : Open-Meteo (CC BY 4.0)';
 
   @override
   String get losLegendRadioHorizon => 'Horizon radio';
